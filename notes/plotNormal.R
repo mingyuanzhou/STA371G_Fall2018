0a24d73b70da986588cdb0a5d44a9949c8bed85a
Mingyuan Zhou <Ming@MSB-MZ5889-W.local>
2018-12-26 12:38:03 -0600

    Update

--- notes/plotNormal.R
+++
@@ -1,9 +0,0 @@
-x = seq(-4,4,0.01)
-plot(x,pnorm(x),type="s")
-lines(x,dnorm(x))
-lines(x,pnorm(x,1,1/2),col="red")
-lines(x,dnorm(x,1,1/2),col="red")
-
-
-plot(x,punif(x),type="s")
-lines(x,dunif(x),col="red")
